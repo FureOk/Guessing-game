74dbc3c2c921bfb03d95b3499f85fb2d22ee2461
f u r e <94113126+FureOk@users.noreply.github.com>
2022-10-21 07:26:18 +0000

    contains code for the game

--- guessinggame.js
+++ guessinggame.js
@@ -3,7 +3,6 @@ console.log(`Welcome to the Guessing Game, ${username}!`)
 
 const minNum = 1;
 let maxNum = 2;
-let points = 0;
 
 function theRange(minNum, maxNum) {
     let randomNumber = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
@@ -11,18 +10,19 @@ function theRange(minNum, maxNum) {
   };
 
 function guessTheNumber(theRange) {
+  console.log("You have a total of 5 lives.")
   let userGuess = parseInt(prompt(`Guess a number in ${theRange}`));
-
-  if (userGuess === randomNumber) {
-    nextGuess = parseInt(prompt('Guess another number: '));
-    if (randomNumber === nextGuess) {
+  let points = 0;
+  let count = 5;
+  for(i = 0; i < count; count ++) {
+    if (userGuess === randomNumber) {
       console.log(`Congratulations ${username}! You won! You can move on to the next stage!`);
       points++;
     } else {
       console.log("Looks like you lost :( Try again.");
     }
   }
+  console.log(`You have a total of ${points} points!`)
 };
-
 
 guessTheNumber(theRange);
